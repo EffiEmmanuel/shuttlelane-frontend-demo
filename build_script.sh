59c0a504e2fd05cb473416b3584560cd76763ea5
EffiEmmanuel <effiemmanuel.n#gmail.com>
2024-04-09 11:36:07 +0100

    NEW: build shell script

---
+++ build_script.sh
@@ -0,0 +1,20 @@
+# run the build command
+set NODE_OPTIONS="--max-old-space-size=16384" && npm run build
+
+# after the build has finished running, go into the build directory
+cd build
+
+# initialize git repository in the build directory
+git init
+
+# add the remote origin
+git remote add origin git@github.com:Shuttlelane-Limited/shuttlelane-frontend-build.git
+
+# stage all the build files
+git add .
+
+# commit build files
+git commit -m "INIT V1.0"
+
+# push build files
+git push -u origin main
